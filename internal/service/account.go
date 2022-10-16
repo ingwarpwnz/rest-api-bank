a499beb932f640131b37be5c4e31d3bbefa4c135
ingwarpwnz <ingwarpwnz@gmail.com>
2022-10-16 17:35:06 +0400

    Added blocking with channels

--- internal/service/account.go
+++ internal/service/account.go
@@ -2,7 +2,6 @@ package service
 
 import (
 	"fmt"
-	"sync"
 
 	"github.com/ingwarpwnz/rest-api-bank/internal/repository/entity"
 )
@@ -15,11 +14,13 @@ type AccountRepositoryInterface interface {
 
 type AccountService struct {
 	repo AccountRepositoryInterface
+	tx   chan struct{}
 }
 
 func NewAccountService(repo AccountRepositoryInterface) *AccountService {
 	return &AccountService{
 		repo: repo,
+		tx:   make(chan struct{}, 1),
 	}
 }
 
@@ -35,27 +36,28 @@ func (s *AccountService) FindById(id string) *entity.Account {
 
 // Transfer funds from one account to another
 func (s *AccountService) Transfer(from, to string, amount float64) (accountFrom *entity.Account, accountTo *entity.Account, err error) {
-	// transaction simulation
-	mu := sync.Mutex{}
-	mu.Lock()
-	defer mu.Unlock()
+	s.tx <- struct{}{}
+	defer func() {
+		<-s.tx
+	}()
 
-	f := s.repo.FindById(from)
-	if f == nil {
+	sender := s.repo.FindById(from)
+	if sender == nil {
 		return nil, nil, fmt.Errorf("sender account not found")
 	}
 
-	if f.Balance-amount < 0 {
+	senderNewBalance := sender.Balance - amount
+	if senderNewBalance < 0 {
 		return nil, nil, fmt.Errorf("the sender has insufficient funds")
 	}
 
-	t := s.repo.FindById(to)
-	if t == nil {
+	recipient := s.repo.FindById(to)
+	if recipient == nil {
 		return nil, nil, fmt.Errorf("recipient account not found")
 	}
 
-	accountFrom = s.repo.UpdateBalance(f.Id, f.Balance-amount)
-	accountTo = s.repo.UpdateBalance(t.Id, t.Balance+amount)
+	accountFrom = s.repo.UpdateBalance(sender.Id, senderNewBalance)
+	accountTo = s.repo.UpdateBalance(recipient.Id, recipient.Balance+amount)
 
 	return accountFrom, accountTo, nil
 }
